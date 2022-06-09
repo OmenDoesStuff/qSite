c4bd3f685c235feb42aa83f6a00beffe4e1d23d7
Polarian <polarian@polarian.dev>
2022-06-09 01:51:33 +0100

    Removed cringe logger and replaced with slf4j api

--- src/main/java/org/minehq/qSite.java
+++ src/main/java/org/minehq/qSite.java
@@ -2,12 +2,8 @@ package org.minehq;
 
 import org.minehq.base.Server;
 import org.minehq.route.main.MainRoute;
-import org.minehq.util.qLogger;
 
 public class qSite {
-
-    private static qLogger logger = new qLogger("qSite");
-
     public static void main(String[] args) {
         Server server = Server.create();
 
@@ -17,9 +13,5 @@ public class qSite {
         server.start(443);
 
 
-    }
-
-    public static qLogger getLogger() {
-        return logger;
     }
 }
